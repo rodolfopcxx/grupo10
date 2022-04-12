66db1ae4330357ce07e41baa4167baedf7e86b63
Rodolfo Peixoto Chaves <rodolfo.chaves@hotmail.com.br>
2022-04-12 19:20:55 -0300

    Fisrt commit

---
+++ src/main/java/grupo10/Grupo10Service/Grupo10ServiceApplication.java
@@ -0,0 +1,13 @@
+package grupo10.Grupo10Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo10ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo10ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo10/Grupo10Service/Grupo10ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo10.Grupo10Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo10ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
